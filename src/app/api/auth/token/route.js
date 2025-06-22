5170f392c1612ceec09c2cfad437267b5469259c
Yasuhiro Maruyama <yasumarunavi@gmail.com>
2025-06-22 16:15:48 +0900

    cookieからトークンを取得する処理追加

--- src/app/api/auth/token/route.js
+++ src/app/api/auth/token/route.js
@@ -1,55 +1,61 @@
 // Next.js API Route(トークン取得BFF) BP002
 "use server";
+import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import redis from "@/lib/redis";
 import { HTTP_STATUS, RESPONSE_CODE, CONTENT_TYPE } from "@/lib/apiConstants";
 import { API_MSG } from "@/lib/messages";
 
-export async function POST(req) {
-  const param = await req.json();
+export async function POST() {
+  // 認証エラーレスポンス
+  const authResponse = {
+    success_flg: false,
+    code: RESPONSE_CODE.AUTH_ERROR,
+    message: API_MSG.authError,
+  };
 
-  // cookieからセッションIDを取得
-  // const cookieStore = await cookies();
-  // const token = cookieStore.get(process.env.COOKIE_TOKEN);
-  // const payload = jwt.verify(token, process.env.JWT_SECRET);
-  // const sessionId = payload.sessionId;
+  // cookieからトークンを取得
+  const cookieStore = await cookies();
+  const token = cookieStore.get(process.env.COOKIE_TOKEN);
 
-  // const redisId = await redis.get(sessionId);
+  // cookieからトークンが取得できない場合、認証エラー
+  if (!token || !token.value) {
+    return NextResponse.json(authResponse, {
+      status: HTTP_STATUS.UNAUTHORIZED,
+    });
+  }
 
-  // cookieが取得できないので、仮設定
-  const userInfo = await redis.get("key");
+  const payload = jwt.verify(token.value, process.env.JWT_SECRET);
+  const sessionId = payload.sessionId;
 
-  // ユーザー情報が取得できなければ認証エラー
+  // セッションIDが取得できない場合、認証エラー
+  if (!payload || !payload.sessionId) {
+    return NextResponse.json(authResponse, {
+      status: HTTP_STATUS.UNAUTHORIZED,
+    });
+  }
+
+  // Redisからユーザー情報を取得
+  const userInfo = await redis.get(sessionId);
+
+  // ユーザー情報が取得できない場合、認証エラー
   if (!userInfo) {
-    return new Response(
-      JSON.stringify({
-        success_flg: false,
-        code: RESPONSE_CODE.AUTH_ERROR,
-        message: API_MSG.authError,
-      }),
-      {
-        status: HTTP_STATUS.UNAUTHORIZED,
-        headers: {
-          "Content-Type": CONTENT_TYPE.APPLICATION_JSON,
-        },
-      }
-    );
+    return NextResponse.json(authResponse, {
+      status: HTTP_STATUS.UNAUTHORIZED,
+    });
   }
 
   // 正常終了(認証OK)
-  return new Response(
-    JSON.stringify({
+  return NextResponse.json(
+    {
       success_flg: true,
       code: RESPONSE_CODE.SUCCESS,
       message: null,
       user_info: userInfo,
-    }),
+    },
     {
       status: HTTP_STATUS.OK,
-      headers: {
-        "Content-Type": CONTENT_TYPE.APPLICATION_JSON,
-      },
     }
   );
 }

31afadecd27f98b4cbd159f2ab26407803f3bdce
Yasuhiro Maruyama <yasumarunavi@gmail.com>
2025-07-19 16:07:39 +0900

    ログイン画面整理

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,54 +1,52 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useReducer } from "react";
 import { toast } from "sonner";
-import { ACTION_TYPE } from "@/lib/uiConstants";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { UI_TYPE } from "@/lib/uiConstants";
 import { UI_MSG } from "@/lib/messages";
-import { b001001Model } from "@/app/model/bff/auth/B001001Model";
 import userModel from "@/app/model/domain/userModel";
-
-// ステート管理
-const reducer = (state, action) => {
-  switch (action.type) {
-    // 項目入力時のイベント
-    case ACTION_TYPE.INPUT:
-      return { ...state, [action.field]: action.payload };
-    // 初期化イベント
-    case ACTION_TYPE.CLEAR:
-      return {};
-    default:
-      return state;
-  }
-};
+import { b001001Model } from "@/app/model/bff/auth/B001001Model";
+import { schema } from "@/validators/auth/U001001Schema";
 
 // ログイン画面
 export default function Page() {
+  // ルーティング用
   const router = useRouter();
-  // 入力項目を保持する
-  const [state, dispatch] = useReducer(reducer, {});
+  // バリデーション用
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   // ログインボタン押下時
-  const loginButton = async (e) => {
-    // デフォルトの送信動作(ページリロードなど)を防ぐ
-    e.preventDefault();
+  const loginButton = async (data) => {
     // B001001_ログイン照会BFF実行
-    const result = await b001001Model(state);
+    const result = await b001001Model(data);
 
     // 異常終了であれば、メッセージを表示して終了
-    if (!result.success_flg) toast.error(UI_MSG.authError);
+    if (!result.success_flg) {
+      toast.error(UI_MSG.authError);
+      return;
+    }
 
     // 正常に返却されれば、ユーザー情報を保存してホーム画面に遷移
     userModel.getState().setUser(result.user_info);
     router.push("/");
+    return;
   };
 
   return (
     <main className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 via-white to-pink-100">
       <div className="w-full max-w-sm p-8 bg-white rounded-2xl shadow-xl">
         <h1 className="text-2xl font-bold text-center mb-6 text-gray-700">
-          ログイン
+          SkoTune AI ログイン
         </h1>
-        <form className="space-y-5" onSubmit={loginButton}>
+        <form className="space-y-5" onSubmit={handleSubmit(loginButton)}>
           <div>
             <label
               htmlFor="user_id"
@@ -57,18 +55,17 @@ export default function Page() {
               メールアドレス
             </label>
             <input
-              type="user_id"
               id="user_id"
-              className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              type={UI_TYPE.E_MAIL}
+              className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 border-gray-300 focus:ring-blue-400"
               placeholder="you@example.com"
-              onChange={(e) => {
-                dispatch({
-                  type: ACTION_TYPE.INPUT,
-                  field: "user_id",
-                  payload: e.target.value,
-                });
-              }}
+              {...register("user_id")}
             />
+            {errors.user_id && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.user_id.message}
+              </p>
+            )}
           </div>
           <div>
             <label
@@ -78,18 +75,17 @@ export default function Page() {
               パスワード
             </label>
             <input
-              type="password"
               id="password"
-              className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              type={UI_TYPE.PASSWORD}
+              className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 border-gray-300 focus:ring-blue-400"
               placeholder="********"
-              onChange={(e) => {
-                dispatch({
-                  type: ACTION_TYPE.INPUT,
-                  field: "password",
-                  payload: e.target.value,
-                });
-              }}
+              {...register("password")}
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
           </div>
           <button
             type="submit"
@@ -100,9 +96,9 @@ export default function Page() {
         </form>
         <p className="mt-4 text-sm text-center text-gray-500">
           アカウントをお持ちでない方は{" "}
-          <a href="#" className="text-blue-500 hover:underline">
+          <Link href="/auth/user" className="text-blue-500 hover:underline">
             登録
-          </a>
+          </Link>
         </p>
       </div>
     </main>
